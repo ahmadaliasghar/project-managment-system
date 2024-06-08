1024a5223382e833d367a146a0497eabf3d7fb0b
Ahmad Ali <ahmadali2882@yahoo.com>
2024-06-08 17:02:09 +0500

    Solved Deployment Issues

--- src/config/http.js
+++ src/config/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://project-managment-system-fvtl.vercel.app/',
     timeout: 10000,
 });
 
